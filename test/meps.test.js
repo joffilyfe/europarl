e419dc8282a4b9dfa2057f39b926d050cc021745
Joffily Ferreira <joffilyferreira@gmail.com>
2024-02-02 16:32:56 +0000

    initial code from politico.eu

---
+++ test/meps.test.js
@@ -0,0 +1,17 @@
+import { scrapeEULegislators } from '../scraper.js'
+
+test('legislators', async () => {
+  const legislators = await scrapeEULegislators()
+
+  // This number can fluctuate, feel free to update the test if it's the case.
+  expect(Array.isArray(legislators)).toBeTruthy()
+  expect(legislators.length).toBe(704)
+
+  const first = legislators[0]
+  expect(first.name).toBe('Magdalena ADAMOWICZ')
+  expect(first.lastName).toBe('ADAMOWICZ')
+  expect(first.partyGroup).toBe('Group of the European People\'s Party (Christian Democrats)')
+  expect(first.country).toBe('Poland')
+  expect(first.url).toBe('https://www.europarl.europa.eu/meps/en/197490/MAGDALENA_ADAMOWICZ/home')
+  expect(first.image).toBe('https://www.europarl.europa.eu/mepphoto/197490.jpg')
+})
